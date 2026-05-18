85cfe6862f8f17fab75fd750bac93761b2b17391
doandrade <dandradecabarcas@gmail.com>
2026-05-18 14:04:46 -0500

    Fix chromium render

--- Bot_Astronorte.js
+++ Bot_Astronorte.js
@@ -49,19 +49,24 @@ const ADMIN = '57TU_NUMERO@c.us';
 // ==========================================
 // CLIENTE WHATSAPP
 // ==========================================
+const { Client, LocalAuth } = require('whatsapp-web.js');
+
 const client = new Client({
-  authStrategy: new LocalAuth(),
-
-  puppeteer: {
-    headless: true,
-
-    args: [
-      '--no-sandbox',
-      '--disable-setuid-sandbox',
-      '--disable-dev-shm-usage',
-      '--disable-gpu'
-    ]
-  }
+    authStrategy: new LocalAuth(),
+    puppeteer: {
+        headless: true,
+        executablePath: process.env.PUPPETEER_EXECUTABLE_PATH || '/usr/bin/chromium',
+        args: [
+            '--no-sandbox',
+            '--disable-setuid-sandbox',
+            '--disable-dev-shm-usage',
+            '--disable-accelerated-2d-canvas',
+            '--no-first-run',
+            '--no-zygote',
+            '--single-process',
+            '--disable-gpu'
+        ]
+    }
 });
 
 // ==========================================
